cbc581f287e8e8ffb8867a33dfb266c6e015a1ab
Dian Sherman <slayer_barrett_@hotmail.com>
2022-10-25 14:10:50 -0700

    moved prompts back to index.

--- index.js
+++ index.js
@@ -9,37 +9,89 @@ const empArray = new Employee[]
 
 const Begin = async () => {
     try {
-        
-       switch (addManager.team){
-        case "Engineer": addEngineer;
-        console.log("add engineer!")
-        break;
-        case "Intern": addIntern;
-        console.log("add intern!")
-        break;
-       }
-       
-            
+        inquirer.prompt([
+            {
+                type: "confirm",
+                message: "Wat is the team member's?",
+                name: "name",
+            }, {
+                type: "input",
+                message: "Enter their employee ID:",
+                name: "id"
+            }, {
+                type: "input",
+                message: "Email address:",
+                name: "email"
+            }
+        ])
+
+        switch (addManager.team) {
+            case "Engineer": addEngineer;
+                console.log("add engineer!")
+                break;
+            case "Intern": addIntern;
+                console.log("add intern!")
+                break;
         }
+
+
+    }
         
     } catch (err) {
-        console.log(err)
-    }
+    console.log(err)
+}
+
+}
+
+function addEmployee() {
 
 }
 
 function addIntern() {
-            
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Please select the school you attend:",
+            name: "school",
+            choices: ["University of Washington", "Central Washington University", "Washington University", "Unversity of Community College"]
+        }, {
+            type: "list",
+            message: "Would you like to add any more team members?",
+            name: ["No", "Engineer", "Intern"]
+        }
+    ])
 }
 
-function addEngineer() {
-    //inqurer
-    //.then() {
-        const engineer = new Engineer(answers.name, a)
-    }
+function addEngineer(answers) {
+    const engineerAns = inquirer.prompt([
+         {
+{
+             type: "input",
+             message: "Github Username:",
+             name: "github"
+         }, {
+             type: "list",
+             message: "Would you like to add any more team members?",
+             name: ["No", "Engineer","Intern"]
+         }
+     ])
+    const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
 }
+
 
 function addManager() {
-
+    inquirer.prompt([ {
+        type: "input",
+            message: "Phone number:",
+                name: "number"
+    }, {
+        type: "list",
+            message: "Please add a team member:",
+                name: "team",
+                    choices: ["Engineer", "Intern"]
+    }
+    ])
 }
+
+
 Begin()
